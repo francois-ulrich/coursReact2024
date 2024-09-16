ae6cf2ee4aedd3f69575a7f60ab13d167bc9f837
François Ulrich <fulrich3@gmail.com>
2024-09-16 10:27:01 +0200

    feat : ajout test-01 et test-02

---
+++ projet-final/spas/test-02/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import GameList from "./GameList";
+
+function App() {
+  return (
+    <div className="App">
+      <GameList />
+    </div>
+  );
+}
+
+export default App;
---
+++ projet-final/spas/test-02/src/GameList.tsx
@@ -0,0 +1,30 @@
+const GameList = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Game</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Super Mario World</td>
+        </tr>
+
+        <tr>
+          <td>2</td>
+          <td>Zelda 3 </td>
+        </tr>
+
+        <tr>
+          <td>3</td>
+          <td>Super Castlevania 4</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default GameList;
